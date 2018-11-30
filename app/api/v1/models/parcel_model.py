119a462685e30d27bd957855be4559625a5cfdfe
wandesky <bwandesky@gmail.com>
2018-11-30 16:29:49 +0300

    [finishes #161819136] create parcel delivery order

--- app/api/v1/models/parcel_model.py
+++ app/api/v1/models/parcel_model.py
@@ -1,8 +1,21 @@
 class Parcel(object):
     parcels = []
 
-    def __init__(self, parcel_id = None):
-        self.parcel_id = parcel_id
+    def __init__(self, placedBy, weight):
+        self.parcel_id = len(Parcel.parcels)
+        self.placedBy = placedBy
+        self.weight = weight
+
 
     def get_all_parcels(self = None):
         return Parcel.parcels
+
+    def post_parcel(self):
+        Parcel.parcels.append(
+            {
+                "id": len(Parcel.parcels),
+                "placedBy": self.placedBy,
+                "weight": self.weight
+            }
+        )
+        return "success"
