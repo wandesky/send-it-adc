19401e8dce46eedf72dedc39e0cffe1cab4b4cd9
wandesky <bwandesky@gmail.com>
2018-12-02 23:16:06 +0300

    clean gh-pages branch

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Blueprint
-from app.api.v1.views.parcel_view import parcels
-from app.api.v1.views.user_view import users
-from app.api.v1.views.edge_view import edges
-
-app = Flask(__name__)
-app.register_blueprint(parcels, url_prefix='/api/v1/parcels')
-app.register_blueprint(users, url_prefix='/api/v1/users')
-app.register_blueprint(edges, url_prefix='/')
